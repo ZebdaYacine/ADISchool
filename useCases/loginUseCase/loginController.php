c6c78286ea8f97aa5b696e2bdd382b935fc8733a
bwiko <bl0x41cko@localhost.localdomain>
2020-12-14 17:04:00 +0100

    pass hash function

--- useCases/loginUseCase/loginController.php
+++ useCases/loginUseCase/loginController.php
@@ -1,8 +1,24 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+<?php 
+/**
+ * 
  */
 
+
+class Login {
+
+	public function hash_pass($pwd) {
+
+		$pepper = "just rendome salt thing for making the hash harder ";
+		$pwd_peppered = hash_hmac("sha256", $pwd, $pepper);
+		$pwd_peppered = md5($pwd_peppered);
+		$pwd_peppered = hash_hmac("sha256", $pwd_peppered, $pepper);
+		$pwd_peppered = md5($pwd_peppered);
+		
+		return $pwd_peppered;
+
+	}
+}
+ $Login_ = new Login() ;
+ $hash_passtest = $Login_->hash_pass("1234");
+ echo $hash_passtest;
+ ?>
