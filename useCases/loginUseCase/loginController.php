77237c7897bc0e099ee78f4cf01fd97d23bd3465
bwiko <bl0x41cko@localhost.localdomain>
2020-12-14 21:37:17 +0100

    add the user verify function

--- useCases/loginUseCase/loginController.php
+++ useCases/loginUseCase/loginController.php
@@ -2,10 +2,30 @@
 /**
  * 
  */
+//Connect to DB
 
 
 class Login {
+	protected $servername = "localhost";
+	protected $username = "root";
+	protected $password = "";
+	protected $dbname = "ADI";
+	function __construct() {
+		
 
+		try {
+		    $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
+		    // set the PDO error mode to exception
+		    $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		 	
+		}
+		catch(PDOException $e) {
+		    echo "Connection failed: " . $e->getMessage();
+		    // and if this fails there is no point continuing so add an exit
+		    exit;
+		}
+
+	}
 	public function hash_pass($pwd) {
 
 		$pepper = "just rendome salt thing for making the hash harder ";
@@ -17,8 +37,36 @@ class Login {
 		return $pwd_peppered;
 
 	}
+	public function CheckUser($usr,$pass)
+	{
+		try {
+			$query="SELECT * FROM `user` WHERE `userName` =:username AND password=:pass";
+			$stmt = $this->conn->prepare($query); 
+			$stmt->bindParam('username', $usr, PDO::PARAM_STR);
+      		$hpass = $this->hash_pass($pass);
+      		$stmt->bindValue('pass',$hpass, PDO::PARAM_STR);
+      		$stmt->execute();
+      		$count = $stmt->rowCount();
+      		$row   = $stmt->fetch(PDO::FETCH_ASSOC);
+		    if($count == 1 && !empty($row)) {
+		       	return True ; 
+		        // /******************** Your code ***********************/
+		        // $_SESSION['sess_user_id']   = $row['uid'];
+		        // $_SESSION['sess_user_name'] = $row['username'];
+		        // $_SESSION['sess_name'] = $row['name'];
+		       
+		      } 
+		    else {
+		    	return False ;
+		      }
+
+
+		} catch (PDOException $e) {
+			 echo "Error : ".$e->getMessage();
+		}
+	}
+
 }
  $Login_ = new Login() ;
- $hash_passtest = $Login_->hash_pass("1234");
- echo $hash_passtest;
+ 
  ?>
