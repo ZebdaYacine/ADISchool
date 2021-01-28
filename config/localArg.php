f1b07739fb8ccfca30475cd2a26fdcf7a988e4b3
ZebdaYacine <36519629+ZebdaYacine@users.noreply.github.com>
2021-01-28 08:11:47 +0100

    revert deleted file

---
+++ config/localArg.php
@@ -0,0 +1,11 @@
+<?php
+
+class localArg {
+
+     static $server_name = "127.0.0.1";
+     static $db_name = "ADI";
+     static $user_name = "root";
+     static $password = "0658185867";
+
+}
+?>
